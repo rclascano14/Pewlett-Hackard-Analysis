9ec7cf1ebe451eeddc50c7cfbf35901b797133e5
rcharleslascano <lascanorc@gmail.com>
2022-02-07 10:07:13 -0600

    Queries.sql and retirement_info

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -40,8 +40,7 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE dept_emp (
@@ -53,3 +52,5 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+
+SELECT * FROM departments;

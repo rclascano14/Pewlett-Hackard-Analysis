2f2123f5c0663bc27a4fbc87437ae4565f7720fd
Charles Lascano <95828604+rclascano14@users.noreply.github.com>
2022-02-07 18:54:01 -0600

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -15,6 +15,8 @@ INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY emp_no ASC, to_date DESC;
 
+SELECT * FROM unique_titles;
+
 --# of Employees by most recent title about to retire
 SELECT COUNT(ut.emp_no), ut.title
 INTO retiring_titles
@@ -38,3 +40,31 @@ ORDER BY emp_no;
 
 SELECT * FROM mentorship_eligibility;
 
+-- Roles in Departments
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no, rt.first_name, rt.last_name, rt.title, d.dept_name
+INTO unique_titles_department
+FROM retirement_titles as rt
+INNER JOIN dept_emp as de
+ON (rt.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (d.dept_no = de.dept_no)
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+--How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO silver_tsunami
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+
+SELECT * FROM silver_tsunami;
+
+--Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO next_generation
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+WHERE ut.title IN ('Senior Engineer', 'Senior Staff', 'Technique Leader', 'Manager')
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+
+SELECT * FROM next_generation;
